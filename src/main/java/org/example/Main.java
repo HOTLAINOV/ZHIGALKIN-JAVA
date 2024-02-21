45a19c35458ac9c5239e18cea895e212854b4790
HOTLAINOV <104721765+HOTLAINOV@users.noreply.github.com>
2024-02-21 19:13:31 +0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод двух строк с клавиатуры
+        
         System.out.println("Введите первую строку:");
         String a = scanner.nextLine();
         System.out.println("Введите вторую строку:");
         String b = scanner.nextLine();
 
-        // Сравнение строк и вывод результата в консоль
+        
         if (a.equals(b)) {
             System.out.println("Строки идентичны");
         } else {
