4d80de97de375b6a588c982c05972ab4ebeabc09
HOTLAINOV <104721765+HOTLAINOV@users.noreply.github.com>
2024-02-21 19:14:34 +0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Заданный массив целых чисел
+        
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Чётные числа:");
 
-        // Перебор элементов массива
+        
         for (int number : numbers) {
-            // Проверка, является ли число чётным
+            
             if (number % 2 == 0) {
-                // Вывод чётного числа в консоль
+                
                 System.out.println(number);
             }
         }
